81061fb4cc239c6b15ae30db88d793fe4ece186f
Souvik Das <svkds1995@gmail.com>
2022-05-22 15:57:59 +0530

    router added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CompanyDetailsComponent } from './components/company-details/company-details.component';
 import { AddCompanyComponent } from './components/add-company/add-company.component';
+import { CompaniesListComponent } from './components/companies-list/companies-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CompanyDetailsComponent,
-    AddCompanyComponent
+    AddCompanyComponent,
+    CompaniesListComponent
   ],
   imports: [
     BrowserModule,
