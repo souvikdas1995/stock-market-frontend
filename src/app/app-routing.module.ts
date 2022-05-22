672a4ac343902d07a7a968e408654f21840888df
moretolearn <yanumularammohan661@gmail.com>
2022-05-22 22:43:04 +0530

    Added CompanyList to Routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CompaniesListComponent } from './components/companies-list/companies-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'company-list', component:CompaniesListComponent},
+  {path:'/',redirectTo: 'company-list', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
