7946f43b1314082c63bb25bcfefbd71c6e0aa879
Souvik Das <svkds1995@gmail.com>
2022-05-23 18:01:46 +0530

    router added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddCompanyComponent } from './components/add-company/add-company.component';
 import { CompaniesListComponent } from './components/companies-list/companies-list.component';
+import { CompanyDetailsComponent } from './components/company-details/company-details.component';
 
 const routes: Routes = [
-  {path:'company-list', component:CompaniesListComponent},
-  {path:'/',redirectTo: 'company-list', pathMatch: 'full'}
+  { path: '', redirectTo: 'companies', pathMatch: 'full' },
+  { path: 'companies', component: CompaniesListComponent },
+  { path: 'companies/:id', component: CompanyDetailsComponent },
+  { path: 'add', component: AddCompanyComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CompanyDetailsComponent } from './components/company-details/company-details.component';
 import { AddCompanyComponent } from './components/add-company/add-company.component';
-import { CompaniesListComponent } from './components/companies-list/companies-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CompanyDetailsComponent,
-    AddCompanyComponent,
-    CompaniesListComponent
+    AddCompanyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
